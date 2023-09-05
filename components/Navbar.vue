e08a440d662be6f543c3a80a37872d33b3c9797d
Gokay Biz <gkymhmt@gmail.com>
2023-09-05 22:38:43 +0200

    fix: navigation url fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -57,7 +57,7 @@
             <li class="mb-4 lg:mb-0 lg:pr-2">
               <NuxtLink
                 class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                to="/"
+                to="/about"
                 >About</NuxtLink
               >
             </li>
