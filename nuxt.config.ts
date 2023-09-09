19ec31d1952c61723f6259e69c8205197d883cbe
Gokay Biz <gkymhmt@gmail.com>
2023-09-09 19:52:18 +0200

    feat: redirect to actual domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ const CONFIG: NuxtConfig = {
   },
 
   vite: {
-    base: "./",
+    base: "/radbiophys/",
     build: {
       sourcemap: false,
     },
---
+++ server/middleware/checkHeader.ts
@@ -0,0 +1,17 @@
+import { getHeader } from "h3";
+
+export default defineEventHandler((event) => {
+  const proxyToken = getHeader(event, "x-proxy-token");
+
+  // Check if the token is missing or incorrect
+  if (
+    !proxyToken ||
+    proxyToken !== "radbiodb-36435d36g8HB0f50B9A3346e50Hd529"
+  ) {
+    return sendRedirect(
+      event,
+      "http://radbiodb.physics.ntua.gr/radbiophys/",
+      301
+    );
+  }
+});
